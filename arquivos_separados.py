b7b8f98fecd3f4742b454881c6dc71d2e83c1fc6
Antonio-RF <antoniidaressurreicao@gmail.com>
2025-03-27 17:46:18 -0300

    Update

---
+++ arquivos_separados.py
@@ -0,0 +1,10 @@
+import re
+
+with open('/home/toninhorf/Documentos/Objdump-Project/arquivos_executaveis.txt', 'r') as file:
+	entradas = file.read()
+
+nomes = re.findall(r'([^\s:]+):', entradas)
+
+with open('/home/toninhorf/Documentos/Objdump-Project/arquivosx.txt', 'w') as file1:
+	file1.write("".join(nomes))
+
